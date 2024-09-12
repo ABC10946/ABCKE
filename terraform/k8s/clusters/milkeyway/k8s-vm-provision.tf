aa521c6cb23feabeee1af3265c8f1ba920ff57f4
ABC10946 <abctech10946@gmail.com>
2024-09-12 20:48:15 +0900

    abcke v2

--- terraform/k8s/clusters/milkeyway/k8s-vm-provision.tf
+++ terraform/k8s/clusters/milkeyway/k8s-vm-provision.tf
@@ -1,82 +1,84 @@
-module "k8s-vega" {
-	source = "../../modules/proxmox/vm"
-	proxmox_host = "vega.element"
-	nodes = [
-#		{
-#			hostname = "k8s-master1"
-#			ipaddr = "192.168.10.91"
-#			memory_size = 4096
-#			disk_size = "10G"
-#		},
-#		{
-#			hostname = "k8s-master2"
-#			ipaddr = "192.168.10.92"
-#			memory_size = 4096
-#			disk_size = "10"
-#		}, 
-	]
-}
-
-# module "k8s-jupiter" {
-# 	source = "../../modules/proxmox/vm"
-# 	proxmox_host = "jupiter.element"
-# 	nodes = [
-# 		{
-# 			hostname = "k8s-worker1"
-# 			ipaddr = "192.168.10.101"
-# 			memory_size = 8192
-# 			disk_size = "50"
-# 		}, 
-# 		{
-# 			hostname = "k8s-worker2"
-# 			ipaddr = "192.168.10.102"
-# 			memory_size = 8192
-# 			disk_size = "50"
-# 		},
-# 		{
-# 			hostname = "k8s-worker3"
-# 			ipaddr = "192.168.10.103"
-# 			memory_size = 8192
-# 			disk_size = "50"
-# 		},
-# 		{
-# 			hostname = "k8s-master3"
-# 			ipaddr = "192.168.10.93"
-# 			memory_size = 4096
-# 			disk_size = "10"
-# 		},
-# 		{
-# 			hostname = "k8s-master4"
-# 			ipaddr = "192.168.10.94"
-# 			memory_size = 4096
-# 			disk_size = "10"
-# 		}
-# 	]
-# }
-
 module "k8s-magnesium" {
 	source = "../../modules/proxmox/vm"
 	proxmox_host = "magnesium.element"
 	nodes = [
 		{
-			hostname = "k8s-master2"
-			ipaddr = "192.168.10.92"
+			hostname = "k8s-master1"
+			ipaddr = "192.168.10.91"
 			memory_size = 4096
-			disk_size = "10"
-		}, 
+			disk_size = "10G"
+		},
+		{
+			hostname = "k8s-worker1"
+			ipaddr = "192.168.10.101"
+			memory_size = 16384
+			disk_size = "10G"
+		},
 	]
 }
 
-module "k8s-vtm" {
+module "k8s-neptune" {
+	source = "../../modules/proxmox/vm"
+	proxmox_host = "neptune.element"
+	nodes = [
+		{
+			hostname = "k8s-master2"
+			ipaddr = "192.168.10.92"
+			memory_size = 4096
+			disk_size = "10G"
+		}, 
+		{
+			hostname = "k8s-worker2"
+			ipaddr = "192.168.10.102"
+			memory_size = 16384
+			disk_size = "10G"
+		},
+	]
+}
+
+module "k8s-uranus" {
+	source = "../../modules/proxmox/vm"
+	proxmox_host = "uranus.element"
+	nodes = [
+ 		{
+ 			hostname = "k8s-master3"
+ 			ipaddr = "192.168.10.93"
+ 			memory_size = 4096
+ 			disk_size = "10G"
+ 		},
+		{
+			hostname = "k8s-worker3"
+			ipaddr = "192.168.10.103"
+			memory_size = 16384
+			disk_size = "10G"
+		},
+	]
+}
+
+module "k8s-vtm1" {
 	source = "../../modules/proxmox/vm"
 	proxmox_host = "magnesium.element"
 	nodetype = "vtm"
 	nodes = [
 		{
-			hostname = "k8s-vtm1"
-			ipaddr = "192.168.10.111"
-			memory_size = 4096
-			disk_size = "10"
+			hostname = "vtm1"
+			ipaddr = "192.168.10.51"
+			memory_size = 2048
+			disk_size = "10G"
+		}
+	]
+}
+
+module "k8s-vtm2" {
+	source = "../../modules/proxmox/vm"
+	proxmox_host = "neptune.element"
+	nodetype = "vtm"
+	nodes = [
+		{
+			hostname = "vtm2"
+			ipaddr = "192.168.10.52"
+			memory_size = 2048
+			disk_size = "10G"
 		}
 	]
 }
