c6f613fb22522be8af5d0eb916183a26788724ed
ABC10946 <abctech10946@gmail.com>
2024-10-09 00:21:08 +0900

    ドメインをelement -> localに変更

--- ansible/reset_ssh_key.sh
+++ ansible/reset_ssh_key.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-master1.element
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-master2.element
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-master3.element
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-worker1.element
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-worker2.element
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-worker3.element
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-master1.local
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-master2.local
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-master3.local
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-worker1.local
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-worker2.local
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R k8s-worker3.local
